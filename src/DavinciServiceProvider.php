3e47dda7ed28bea242a2d9abba5b300b86e06d4e
Zachary Schuessler <zlschuessler@gmail.com>
2018-08-26 10:42:37 -0700

    initial setup for packagist

---
+++ src/DavinciServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Davinci\Davinci;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+
+    }
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
